89a3d2c2cf518364ba24aac220087ca4c9c0e3dd
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2019-09-26 18:06:18 +0300

    Слайдер наша команда

--- build/js/main.js
+++ build/js/main.js
@@ -13,3 +13,73 @@
     }
   }
 })();
+
+'use strict';
+
+(function () {
+
+  var slides = document.querySelectorAll('.reviews__item');
+  var next = document.querySelector('.reviews__slider button:first-of-type');
+  var previous = document.querySelector('.reviews__slider button:last-of-type');
+  var currentSlide = 0;
+  var slideInterval = setInterval(nextSlide,2000);
+
+  function nextSlide() {
+    goToSlide(currentSlide + 1);
+  }
+
+  function previousSlide() {
+    goToSlide(currentSlide - 1);
+  }
+
+  function goToSlide(n) {
+    slides[currentSlide].className = 'reviews__item';
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].className = 'reviews__item reviews__item-show';
+  }
+
+  next.addEventListener('click', function () {
+    nextSlide();
+    clearInterval(slideInterval);
+  });
+  previous.addEventListener('click', function () {
+    previousSlide();
+    clearInterval(slideInterval);
+  });
+
+})();
+
+'use strict';
+
+(function () {
+
+  var slides = document.querySelectorAll('.team__item');
+  var next = document.querySelector('.team__slider button:first-of-type');
+  var previous = document.querySelector('.team__slider button:last-of-type');
+  var currentSlide = 0;
+  var slideInterval = setInterval(nextSlide,2000);
+
+  function nextSlide() {
+    goToSlide(currentSlide + 1);
+  }
+
+  function previousSlide() {
+    goToSlide(currentSlide - 1);
+  }
+
+  function goToSlide(n) {
+    slides[currentSlide].className = 'team__item';
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].className = 'team__item team__item-show';
+  }
+
+  next.addEventListener('click', function () {
+    nextSlide();
+    clearInterval(slideInterval);
+  });
+  previous.addEventListener('click', function () {
+    previousSlide();
+    clearInterval(slideInterval);
+  });
+
+})();
